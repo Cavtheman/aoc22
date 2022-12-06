03810e83328d74c05f76bc6f50d3d4c9fcf9fff6
Cavtheman <casper_lf@yahoo.dk>
2022-12-06 14:56:10 +0100

    spent way too much time on day 6, trying to fix an issue that wasnt there

---
+++ day06/day6.hs
@@ -0,0 +1,24 @@
+import Data.List
+
+readLines :: FilePath -> IO [String]
+readLines = fmap lines . readFile
+
+isUnique :: String -> String -> Int -> Bool
+isUnique _ _ 0 = True
+isUnique (x:xs) seen n | notElem x seen = isUnique xs (x:seen) $ n - 1
+isUnique _ _ _ = False
+
+findAnyUnique :: Int -> String -> Maybe Int
+findAnyUnique n s =
+  let helper s' index | isUnique s' "" n = Just $ index + n
+      helper (_:xs) index = helper xs $ index + 1
+      helper _ _ = Nothing
+  in helper s 0
+
+main :: IO ()
+main = do
+  contents <- readLines "input.txt"
+  let results1 = map (findAnyUnique 4) contents
+      results2 = map (findAnyUnique 14) contents
+  print results1
+  print results2
